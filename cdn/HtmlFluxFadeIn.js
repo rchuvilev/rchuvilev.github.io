296e00b4b4044ab3cae1d924c25b689ca815ef10
roman.chuvilev <roman.chuvilev@arkadium.com>
2025-02-10 19:51:20 +0400

    Updates

--- cdn/HtmlFluxFadeIn.js
+++ cdn/HtmlFluxFadeIn.js
@@ -1,1 +1,1 @@
-document.body.style.opacity="0";document.body.style.transition="opacity 1000ms ease-in";window.addEventListener("load",()=>{document.body.setAttribute("style","opacity: 1")});
+document.body.style.opacity="0";document.body.style.transition="opacity 1000ms ease-in";window.addEventListener("load",()=>{document.body.style.opacity="1",setTimeout(()=>{document.body.style.opacity="",document.body.style.transition=""},2000)});
